c4ce72ada4ae213c60d2714b4a2e501b8b6810b8
seekdestiny <jifeiqian@gmail.com>
2016-01-20 22:26:34 -0800

    initialize the game board

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+	this.x = 100;
+	this.y = 100;
 };
 
 // Update the enemy's position, required method for game
@@ -24,12 +26,43 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite ='images/char-boy.png';
+	this.x = 2 * 101;
+	this.y = 4 * 83 + 50;
+};
 
+Player.prototype.update = function(dt) {
+
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(mvDrctn) {
+	if (mvDrctn == 'left') {
+        this.x -= 101;
+	}
+    if (mvDrctn == 'right') {
+        this.x += 101;
+	}
+    if (mvDrctn == 'up') {
+        this.y -= 83;
+	}
+	if (mvDrctn == 'down') {
+        this.y += 83;
+	}
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var enemy1 = new Enemy();
+var allEnemies = [enemy1];
 
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
